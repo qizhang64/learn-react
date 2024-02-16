6dc583b2d38f542159d99da5becda2218ceef24a
张琪 <qizhang@zhangqidembp.lan>
2024-02-16 15:17:56 -0500

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './qcomps/profile_mistake'
+import MyComp from './qcomps/firstcomp'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <MyComp />
     </div>
   )
 }
